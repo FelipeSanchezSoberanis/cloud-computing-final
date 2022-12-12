5870633334be3cef84f95cf023456182eae034d2
Felipe Sánchez Soberanis <felipesaso2000@gmail.com>
2022-12-12 10:00:10 -0600

    Initial commit

---
+++ src/test/java/com/cloudcomputing/finalproyect/FinalproyectApplicationTests.java
@@ -0,0 +1,11 @@
+package com.cloudcomputing.finalproyect;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FinalproyectApplicationTests {
+
+    @Test
+    void contextLoads() {}
+}
